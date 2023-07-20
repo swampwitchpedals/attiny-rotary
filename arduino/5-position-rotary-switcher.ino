4313c4dc7edda1d75381d86542eaa435e7c5e841
Chris Nielubowicz <nielubowicz@gmail.com>
2023-07-20 17:34:35 -0400

    Update code for arduino sketches

--- arduino/5-position-rotary-switcher.ino
+++ arduino/5-position-rotary-switcher.ino
@@ -1,10 +1,12 @@
 #define POSITION A0
-#define LED 13
 
+byte lastPin = 0xAA;
+const int threshold = 10;
 const int selectPins[3] = {A13, A14, A15}; // S-pins to Arduino pins: S0~2, S1~3, S2~4
-const int totalPositions = 5;
 
 void setup() {
+  // put your setup code here, to run once:
+  // Serial.begin(9600); // uncomment to debug
   pinMode(POSITION, INPUT);
   pinMode(A13, OUTPUT);
   pinMode(A14, OUTPUT);
@@ -12,16 +14,39 @@ void setup() {
 }
 
 void loop() {
+
   int position = analogRead(POSITION);
-  float shiftedPosition = ((position >> 7) / 2.0); 
-  int pin = ceil(shiftedPosition);
-  selectMuxPin(pin);    
+
+  // The following line works for 5 positions)
+  const int totalPositions = 5;
+  const int expectedValues[] = {985, 767, 493, 220, 0};
+  
+  for (int i = 0; i < totalPositions; i++) {
+    if (evaluatePosition(position, expectedValues[i], threshold)) {
+      selectMuxPin(totalPositions - 1 - i);    
+      break;
+    }
+  }
 }
 
+// Returns true if the expected position is within 1 of the given value
+bool evaluatePosition(int position, int expectedValue, int threshold) {
+  if (expectedValue == 0 && position == 0) {
+    return true;
+  }
 
+  return (expectedValue <= (position + threshold) && 
+      expectedValue >= (position - threshold));
+}
 // The selectMuxPin function sets the S0, S1, and S2 pins to select the give pin
 void selectMuxPin(byte pin) {
+  if (pin == lastPin) return; // Exit if we have already set this pin
   if (pin > 7) return; // Exit if pin is out of scope
+  lastPin = pin;
+
+  Serial.print("Selecting a pin: ");
+  Serial.println(pin);
+
   for (int i=0; i<3; i++) {
     if (pin & (1<<i))
       digitalWrite(selectPins[i], HIGH);
@@ -29,3 +54,4 @@ void selectMuxPin(byte pin) {
       digitalWrite(selectPins[i], LOW);
   }
 }
+
--- arduino/8-position-rotary-switcher.ino
+++ arduino/8-position-rotary-switcher.ino
@@ -1,10 +1,12 @@
 #define POSITION A0
-#define LED 13
 
+byte lastPin = 0xAA;
+const int threshold = 10;
 const int selectPins[3] = {A13, A14, A15}; // S-pins to Arduino pins: S0~2, S1~3, S2~4
-const int totalPositions = 8;
 
 void setup() {
+  // put your setup code here, to run once:
+  // Serial.begin(9600); // uncomment to debug
   pinMode(POSITION, INPUT);
   pinMode(A13, OUTPUT);
   pinMode(A14, OUTPUT);
@@ -14,19 +16,36 @@ void setup() {
 void loop() {
   int position = analogRead(POSITION);
 
-  float shiftedPosition = ((position >> 6) / 2.0); 
-  if (shiftedPosition > totalPositions / 2.0) {
-    shiftedPosition -= 1;
+  // The following lines work for 8 positions
+  const int totalPositions = 8;
+  const int expectedValues[] = {987, 895, 742, 581, 416, 253, 93, 0};
+  
+  for (int i = 0; i < totalPositions; i++) {
+    if (evaluatePosition(position, expectedValues[i], threshold)) {
+      selectMuxPin(totalPositions - 1 - i);    
+      break;
+    }
   }
-
-  int pin = ceil(shiftedPosition);
-  selectMuxPin(pin);    
 }
 
+// Returns true if the expected position is within 1 of the given value
+bool evaluatePosition(int position, int expectedValue, int threshold) {
+  if (expectedValue == 0 && position == 0) {
+    return true;
+  }
 
+  return (expectedValue <= (position + threshold) && 
+      expectedValue >= (position - threshold));
+}
 // The selectMuxPin function sets the S0, S1, and S2 pins to select the give pin
 void selectMuxPin(byte pin) {
+  if (pin == lastPin) return; // Exit if we have already set this pin
   if (pin > 7) return; // Exit if pin is out of scope
+  lastPin = pin;
+
+  Serial.print("Selecting a pin: ");
+  Serial.println(pin);
+
   for (int i=0; i<3; i++) {
     if (pin & (1<<i))
       digitalWrite(selectPins[i], HIGH);
@@ -34,3 +53,4 @@ void selectMuxPin(byte pin) {
       digitalWrite(selectPins[i], LOW);
   }
 }
+
